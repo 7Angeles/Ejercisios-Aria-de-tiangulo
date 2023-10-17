e3ba5e16828b609661c92944edd995881751f5ad
angel <angel@LAPTOP-ISRN8DB7>
2023-10-16 21:01:27 -0600

    Agregar archivos de proyecto.

---
+++ Formulario/AreaTiangulo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercisios.Formulario
+{
+    public partial class AreaTiangulo : Form
+    {
+        public AreaTiangulo()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonResultado_Click(object sender, EventArgs e)
+        {
+            try { 
+            float Base = float.Parse(textoBase.Text);
+            float Altura = float.Parse(textoAltura.Text);
+
+            if(Base>= 0 && Altura>= 0)
+            {
+                float resultado = (Base * Altura) / 2;
+
+                textoResultado.Text = "" + resultado;
+            }else
+            {
+                MessageBox.Show(this, "datos incorectos", "Error");
+            }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(this, "Datos no numericos", "Datos incorectos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+
+
+
+
+
+    }
+    }
+}
